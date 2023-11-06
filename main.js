d38884a171fd1011cf6925847bd470f509afdf37
Branson <104116765+5uggz@users.noreply.github.com>
2023-11-06 10:40:00 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+def saveInfo():
+    #Saves user Info to txt pad
+    print("Username:", username, "Password:", password)
+
+def robux():
+    #Visible Robux giving print
+    print("Giving", username, robuxAmount, "Robux!")
+
+# Visible to User
+print(" !?  Infinite Robux Generator  !?")
+robuxAmount = input("$ Amount of Robux $: ")
+username = input("/ Username /: ")
+password = input("< Password >: ")
+robux()
